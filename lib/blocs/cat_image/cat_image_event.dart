8f6ec0479b8f15a8e5cf1528dd5318b362d8475e
Mirkan <mirkancaliskan@gmail.com>
2023-04-28 11:30:18 +0400

    remove unnecessary bloc

--- lib/blocs/cat_image/cat_image_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'cat_image_bloc.dart';
-
-@freezed
-class CatImageEvent with _$CatImageEvent {
-  const factory CatImageEvent.newImage() = _NewImage;
-}
--- lib/blocs/cat_image/cat_image_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'cat_image_bloc.dart';
-
-@freezed
-class CatImageState with _$CatImageState {
-  const factory CatImageState.loading() = _Initial;
-  const factory CatImageState.loaded(String url) = _Loaded;
-}
